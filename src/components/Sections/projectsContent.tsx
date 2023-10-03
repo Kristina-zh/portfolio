9961dc7416f87389c2ba9b4d12026b4afb559ce1
Kristina <kristinazhuravleva@ballastlane.com>
2023-10-02 20:45:44 -0600

    resilved failed build problem

--- src/components/Sections/projectsContent.tsx
+++ src/components/Sections/projectsContent.tsx
@@ -1,18 +1,18 @@
 export const projectsList = [
   {
+    id: '02',
+    title: "Financial Widget",
+    description: "Usefull calculator to help with planning finance and big purchases",
+    image: "/images/tailwindcss.png",
+    skills: ["Bootstrap", "React", "Typescript", "NextJS"],
+    link: "https://tailwindcss-project-by-kristina.netlify.app/"
+  },
+  {
     id: '01',
     title: "Landing Page",
     description: "Responcive modern web-site, implemented theme switcher by using TailwindCSS technology.",
     image: "/images/tailwindcss.png",
     skills: ["HTML5", "Javascript", "TailwindCSS", "Netlify"],
-    link: "https://tailwindcss-project-by-kristina.netlify.app/"
-  },
-  {
-    id: '02',
-    title: "Financual widget",
-    description: "Usefull calculator to help with planning finance and big purchases",
-    image: "/images/tailwindcss.png",
-    skills: ["Bootstrap", "React", "Typescript", "NextJS"],
     link: "https://tailwindcss-project-by-kristina.netlify.app/"
   }
 ]
