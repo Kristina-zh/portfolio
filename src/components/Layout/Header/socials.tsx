05e5ce99c850201d26df4da7a883f78e4b08cb97
Kristina <kristinazhuravleva@ballastlane.com>
2023-10-02 21:07:50 -0600

    temporary disabled social icons

--- src/components/Layout/Header/socials.tsx
+++ src/components/Layout/Header/socials.tsx
@@ -1,21 +1,24 @@
-import LinkedinIcon from '@mui/icons-material/Linkedin';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+// import LinkedinIcon from '@mui/icons-material/Linkedin';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export const contacts = [
   {
     name: 'github',
-    icon: <GitHubIcon sx={{ fontSize: 28 }} />,
+    icon: null,
+    // icon: <GitHubIcon sx={{ fontSize: 28 }} />,
     link: 'https://github.com/Kristina-zh',
   },
   {
     name: 'whatsup',
-    icon: <WhatsAppIcon sx={{ fontSize: 28 }} />,
+    icon: null,
+    // icon: <WhatsAppIcon sx={{ fontSize: 28 }} />,
     link: '',
   },
   {
     name: 'linkedin',
-    icon: <LinkedinIcon sx={{ fontSize: 28 }} />,
+    icon: null,
+    // icon: <LinkedinIcon sx={{ fontSize: 28 }} />,
     link: 'https://www.linkedin.com/in/kristina-zhuravleva-a0624619a/',
   }
 ]
