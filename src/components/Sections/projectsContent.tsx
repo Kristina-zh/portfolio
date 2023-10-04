2f9068290298efd4460611a034c72da7acc4f848
Kristina <kristinazhuravleva@ballastlane.com>
2023-10-04 10:18:13 -0600

    added image for fin project

--- src/components/Sections/projectsContent.tsx
+++ src/components/Sections/projectsContent.tsx
@@ -3,7 +3,7 @@ export const projectsList = [
     id: '020',
     title: "Financial Widget",
     description: "Usefull calculator to help to plan big purchases",
-    image: "/images/tailwindcss.png",
+    image: "/images/calculator.jpg",
     skills: ["Bootstrap", "React", "Typescript", "NextJS"],
     link: "https://tailwindcss-project-by-kristina.netlify.app/"
   },
@@ -11,7 +11,7 @@ export const projectsList = [
     id: '01',
     title: "Landing Page",
     description: "Responcive modern web-site, implemented theme switcher by using TailwindCSS technology.",
-    image: "/images/tailwindcss.png",
+    image: "/images/tailwindcss.jpg",
     skills: ["HTML5", "Javascript", "TailwindCSS", "Netlify"],
     link: "https://tailwindcss-project-by-kristina.netlify.app/"
   }
